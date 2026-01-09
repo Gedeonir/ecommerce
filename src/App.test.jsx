89e3ef8851ea6582b333ec78ef291c63256a3eaf
Gedeon <irafasha.jedy12@gmail.com>
2026-01-09 09:40:32 +0200

    Fourth commit

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,9 @@
 import { render} from "@testing-library/react";
 import App from "./App";
-import { test } from "vitest";
+import { expect, test } from "vitest";
 import '@testing-library/jest-dom'
 
 test("renders without crashing", () => {
   render(<App />)
+  expect(screen.getByText("GOLFGRIDS.")).toBeInTheDocument();
 });
